8a8badbf87b3d72c3fda036a9ae11a72442c2a86
kuza11 <kuza2017@seznam.cz>
2024-11-09 20:07:04 +0100

    xss

--- kontrolery/Kontroler.php
+++ kontrolery/Kontroler.php
@@ -9,19 +9,35 @@ abstract class Kontroler
     protected array $hlavicka = array('titulek' => '', 'klicova_slova' => '', 'popis' => '');
 
     abstract function zpracuj(array $parametry): void;
-    
+
     public function vypisPohled(): void
     {
         if ($this->pohled) {
             extract($this->data);
+            extract($this->data, EXTR_PREFIX_ALL, "");
             require("pohledy/" . $this->pohled . ".phtml");
         }
     }
-    
+
     public function presmeruj(string $url): never
     {
         header("Location: /$url");
         header("Connection: close");
         exit;
+    }
+
+    private function osetri(mixed $x = null): mixed
+    {
+        if (!isset($x))
+            return null;
+        elseif (is_string($x))
+            return htmlspecialchars($x, ENT_QUOTES);
+        elseif (is_array($x)) {
+            foreach ($x as $k => $v) {
+                $x[$k] = $this->osetri($v);
+            }
+            return $x;
+        } else
+            return $x;
     }
 }
